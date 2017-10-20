884eeae49b1058c1a408e009c15980eab1750e4f
climbingcode <thomas.davis@widerfunnel.com>
2017-10-19 21:14:37 -0700

    Change order of asset compile config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
@@ -28,12 +31,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  # config.serve_static_assets = true
-  # config.assets.digest = true
-
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
