b9a0ffcce4e07d1fc325e2996dc12f5e1e94adbb
wf@widerfunnel.com <thomas.davis@widerfunnel.com>
2018-10-21 23:21:23 -0700

    Skip token on api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,8 @@ class ApiController < ApplicationController
 
 	protect_from_forgery with: :null_session
 
+	skip_before_action :verify_authenticity_token
+
 	serialization_scope :current_user
 
 	#returns the active user associated with the access token
