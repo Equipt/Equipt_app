cb234bd399c8c1866f1799be41ea44fc0502f677
wf@widerfunnel.com <thomas.davis@widerfunnel.com>
2018-11-11 16:48:43 -0800

    Address is done

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,27 +2,34 @@ class Address < ApplicationRecord
 
   has_one :user, dependent: :destroy
 
-  before_validation :geocode, :unless => :verified
+  # before_validation :geocode, :unless => :verified
 
   validates_presence_of :number, :street, :city, :state, :zip, :country
 
-  validate :real_address?
+  before_save :real_address
 
-  before_save :real_address? unless :skip_geocoded_valiation
-
-  geocoded_by :full_address do |address, results|
-    if results.present?
-      address.latitude = results.first.latitude
-      address.longitude = results.first.longitude
-      address.verified = true
-    end
+  def real_address
+    address.verified = true
   end
 
-  def real_address?
-    return true if verified
-    errors.add(:invalid, I18n.t('user.invalid_address'))
-    false
-  end
+  # validate :real_address?
+
+  # before_save :real_address? unless :skip_geocoded_valiation
+
+  # geocoded_by :full_address do |address, results|
+    # binding.remote_pry
+  #   if results.present?
+  #     address.latitude = results.first.latitude
+  #     address.longitude = results.first.longitude
+  #     address.verified = true
+  #   end
+  # end
+
+  # def real_address?
+  #   return true if verified
+  #   errors.add(:invalid, I18n.t('user.invalid_address'))
+  #   false
+  # end
 
   def full_address
     [number, street, city, state, zip, country].join(',')
