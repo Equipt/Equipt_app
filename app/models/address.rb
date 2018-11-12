2a9a6140d5ae6e10103c0486b00b4fdbf1681782
wf@widerfunnel.com <thomas.davis@widerfunnel.com>
2018-11-12 15:47:14 -0800

    Missing api key for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,34 +2,27 @@ class Address < ApplicationRecord
 
   has_one :user, dependent: :destroy
 
-  # before_validation :geocode, :unless => :verified
+  before_validation :geocode, :unless => :verified
 
   validates_presence_of :number, :street, :city, :state, :zip, :country
 
-  before_save :real_address
+  validate :real_address?
 
-  def real_address
-    address.verified = true
+  before_save :real_address? unless :skip_geocoded_valiation
+
+  geocoded_by :full_address do |address, results|
+    if results.present?
+      address.latitude = results.first.latitude
+      address.longitude = results.first.longitude
+      address.verified = true
+    end
   end
 
-  # validate :real_address?
-
-  # before_save :real_address? unless :skip_geocoded_valiation
-
-  # geocoded_by :full_address do |address, results|
-    # binding.remote_pry
-  #   if results.present?
-  #     address.latitude = results.first.latitude
-  #     address.longitude = results.first.longitude
-  #     address.verified = true
-  #   end
-  # end
-
-  # def real_address?
-  #   return true if verified
-  #   errors.add(:invalid, I18n.t('user.invalid_address'))
-  #   false
-  # end
+  def real_address?
+    return true if verified
+    errors.add(:invalid, I18n.t('user.invalid_address'))
+    false
+  end
 
   def full_address
     [number, street, city, state, zip, country].join(',')
